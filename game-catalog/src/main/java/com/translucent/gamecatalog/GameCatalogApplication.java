826387ee97cb2543d206ebbd474f261634be2b8c
Romulo Pereira <romulo.pereiracz@gmail.com>
2021-08-27 09:59:36 -0300

    uploading project with model class, repository, M2 bank settings and model creation

---
+++ game-catalog/src/main/java/com/translucent/gamecatalog/GameCatalogApplication.java
@@ -0,0 +1,29 @@
+package com.translucent.gamecatalog;
+
+import com.translucent.gamecatalog.model.Game;
+import com.translucent.gamecatalog.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class GameCatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Game game = new Game( null,"game", "2011", "video game", true, LocalDate.now(), "great");
+		Game game1 = new Game(null, "game1", "20121", "video game1", true, LocalDate.now(), "great 1");
+		gameRepository.saveAll(Arrays.asList(game, game1));
+	}
+}
---
+++ game-catalog/src/main/java/com/translucent/gamecatalog/model/Game.java
@@ -0,0 +1,42 @@
+package com.translucent.gamecatalog.model;
+
+import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "error.validation.title.not.null")
+    @Size(max = 100, message = "error.validation.title.sizeTitle")
+    private String title;
+
+    @NotNull(message = "error.validation.year.not.null")
+    private String year;
+
+    @NotNull(message = "error.validation.console.not.null")
+    private String console;
+
+    @NotNull(message = "error.validation.completed.not.null")
+    private Boolean completed;
+
+    private LocalDate dateOfCompletion;
+
+    @NotNull(message = "error.validation.personalNotes.not.null")
+    private String personalNotes;
+
+}
