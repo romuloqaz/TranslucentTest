9a2ffb7c576cf4ed95d387ccc433f6863f453979
Romulo Pereira <romulo.pereiracz@gmail.com>
2021-08-30 17:59:25 -0300

    including api documentation with swagger

--- game-catalog/src/main/java/com/translucent/gamecatalog/controller/GameController.java
+++ game-catalog/src/main/java/com/translucent/gamecatalog/controller/GameController.java
@@ -3,6 +3,9 @@ package com.translucent.gamecatalog.controller;
 import com.translucent.gamecatalog.exception.ValidationException;
 import com.translucent.gamecatalog.model.Game;
 import com.translucent.gamecatalog.service.GameService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,12 +25,21 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @ApiOperation("Get list of the games")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Returns a list of games with the status OK"),
+    })
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<Game>>findAllGamesByDate() {
         List<Game> list = gameService.findAllOrderByDate();
         return ResponseEntity.ok().headers(getHttpHeaders()).body(list);
     }
 
+    @ApiOperation("Creating a new game")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Return an empty list with the status Created"),
+            @ApiResponse(code = 400, message = "Invalid request attributes"),
+    })
     @RequestMapping(method=RequestMethod.POST)
     public ResponseEntity<Game> insert(@Valid @RequestBody Game obj, Errors errors) {
         if (errors.hasErrors()) {
