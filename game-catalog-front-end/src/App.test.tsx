92f92a478bb440f57adb14d14e01e2b903a319ed
Romulo Pereira <romulo.pereiracz@gmail.com>
2021-09-06 16:00:46 -0300

    add store tests

--- game-catalog-front-end/src/App.test.tsx
+++ game-catalog-front-end/src/App.test.tsx
@@ -1,10 +1,60 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
+import * as redux from 'react-redux';
 
-import App from './App';
+import {
+  fetchGamesFailure,
+  fetchGamesRequest,
+  fetchGamesSuccess,
+} from './store/games/gamesActions';
+import reducer from './store/games/reducer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('test state aplication', () => {
+  const initialState = {
+    loading: true,
+    games: [],
+    error: null,
+    loaded: false,
+  };
+
+  const fetchState = {
+    loading: false,
+    games: [
+      {
+        id: 1,
+        title: 'Metal Gear Solid 2',
+        year: 2001,
+        console: 'PS2',
+        completed: false,
+        dateOfCompletion: null,
+        personalNotes:
+          ' I really liked this game. A masterpiece from Kojima productions.',
+      },
+    ],
+    error: null,
+    loaded: false,
+  };
+
+  const fetchStateFailed = {
+    loading: false,
+    games: [],
+    error: 'error',
+    loaded: false,
+  };
+
+  test('test initial state', () => {
+    expect(reducer(initialState, fetchGamesRequest()));
+  });
+
+  test('test fetch request success', () => {
+    expect(reducer(initialState, fetchGamesSuccess(fetchState)));
+  });
+
+  test('test fetch request failed', () => {
+    expect(reducer(initialState, fetchGamesFailure(fetchStateFailed)));
+  });
+
+  test('test mock useSelector', () => {
+    jest
+      .spyOn(redux, 'useSelector')
+      .mockImplementation((callback) => callback(initialState));
+  });
 });
